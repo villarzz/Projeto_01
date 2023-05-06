ceb383323c062e977dc41a8aea39ce4a19f31d2c
Hugo Vilar <hugovilar04@gmail.com>
2023-05-06 16:56:21 -0300

    declaração de variaveis

---
+++ projeto.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int id[1000];
+    char nomeCompleto[1000][40];
+    char email[1000][50];
+    char sexo[1000][11];
+    char endereco[1000][50];
+    double altura [1000];
+    int vacina[1000][1];
+}
