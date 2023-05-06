4e165e51c4956f94719a36ec0d387ac41f7fa7a1
Hugo Vilar <hugovilar04@gmail.com>
2023-05-06 19:30:08 -0300

    recebendo dados dos usuarios

--- projeto.c
+++ projeto.c
@@ -1,38 +1,53 @@
 #include <stdio.h>
+#include <string.h>
 
-void incluiUsuario(int *id, char *nomeCompleto,char *email, char *sexo, char *endereco, double *altura, int *vacina){
+void incluiUsuario(int *id, char nomeCompleto[][50],char email[][50], char sexo[][50], char endereco[][50], double *altura, int *vacina){
     int i;
-    for (i = 0; i <= 5; i++){
+    for (i = 0; i < 5; i++){
         printf("Digite o codigo do usuario:\n");
         scanf("%d", &id[i]);
+        getchar();
 
         printf("Digite o nome completo do usuario:\n");
-        scanf("%s", &nomeCompleto[i]);
+        fgets(nomeCompleto[i],50, stdin);
+        strtok(nomeCompleto[i], "\n");
 
         printf("Digite o email do usuario:\n");
-        scanf("%s", &email[i]);
+        fgets(email[i], 50, stdin);
+        strtok(email[i], "\n");
 
         printf("Digite o sexo do usuario:\n");
-        scanf("%s", &sexo[i]);
+        scanf("%s", sexo[i]);
+        getchar();
 
         printf("Digite o endereco do usuario:\n");
-        scanf("%s", &endereco[i]);
+        fgets(endereco[i], 50, stdin);
+        strtok(endereco[i], "\n");
 
         printf("Digite a altura do usuario:\n");
         scanf("%lf", &altura[i]);
+        getchar();
 
         printf("O usuario esta vacinado:\n");
-        scanf("%d", vacina[i]);
+        scanf("%d", &vacina[i]);
+        getchar();
     }
     
 }
 
 int main(){
-    int id[5];
-    char nomeCompleto[5][40];
+    int id[5],i;
+    char nomeCompleto[5][50];
     char email[5][50];
-    char sexo[5][11];
+    char sexo[5][50];
     char endereco[5][50];
     double altura [5];
-    int vacina[5][1];
+    int vacina[5];
+
+    incluiUsuario(id,nomeCompleto,email,sexo,endereco,altura,vacina);
+    for(i=0;i<5;i++){
+        printf("%d - %s- %s- %s- %s- %.2lf- %d\n", id[i], nomeCompleto[i], email[i], sexo[i], endereco[i], altura[i], vacina[i]);
+    }
+
+    return 0;
 }
